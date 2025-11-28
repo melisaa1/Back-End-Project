52e8f59069b1bb1bbf3f77a4b3b6d3535f5f6844
Rahmah <170751500+rahmaAkko@users.noreply.github.com>
2025-11-28 17:43:28 +0300

    trying again

--- obj/Debug/net8.0/RateNowApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/RateNowApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RateNowApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ddf0c98bc3c103868352af17fde48aa3dbe5ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("RateNowApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RateNowApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
