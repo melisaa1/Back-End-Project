92907717e0541717d05c66a13c7f91f3b12c73d2
Rahmah <170751500+rahmaAkko@users.noreply.github.com>
2025-12-04 12:05:03 +0300

    merge with melisa

--- obj/Debug/net8.0/RateNowApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/RateNowApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RateNowApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d608ee0b6c4264c0b5dd3df54879a1232047408c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd27156dec231755dfbbbb9eacccc98c7adcbdc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("RateNowApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RateNowApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
