c988d03e1cb7c35710a03a5617bc9051b61c4b6d
melisaa1 <melisapekemen.2001@gmail.com>
2025-12-09 15:50:53 +0300

    save changes

--- obj/Debug/net8.0/RateNowApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/RateNowApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RateNowApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ab3600daa7c55a6fdeaecd9e6013529d4531331")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6518247ee6dbf30033d320cf67bc0be7a1276ab3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RateNowApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RateNowApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
