59c28cc5022627249ae52e7806706649cee87b47
melisaa1 <melisapekemen.2001@gmail.com>
2025-12-18 17:04:34 +0300

    connect controllers with DTOs

--- DTOs/UserResponseDto.cs
+++ DTOs/UserResponseDto.cs
@@ -3,8 +3,8 @@ namespace RateNowApi.DTOs.Users
     public class UserResponseDto
     {
         public int Id { get; set; }
-        public string UserName { get; set; } = null!;
-        public string Email { get; set; } = null!;
-        public string Role { get; set; } = null!;
+        public string? UserName { get; set; } = null!;
+        public string? Email { get; set; } = null!;
+        public string? Role { get; set; } = null!;
     }
 }
